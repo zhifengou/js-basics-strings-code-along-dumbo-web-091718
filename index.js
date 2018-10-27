fa486057a4924be27de46bf0d1694e37a4dc47d3
zhifeng ou <zhifengou@hotmail.com>
2018-10-27 17:10:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Avi";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+let shortGreeting = "Welcome, " + currentUser[0] + "!";
